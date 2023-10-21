d20701ccd074f94d8012ec8292b4f012ff50a8d5
mwhite90Booth <146589669+mwhite90Booth@users.noreply.github.com>
2023-10-21 13:41:00 +0000

    Rock Paper Scissors Logic

--- app.rb
+++ app.rb
@@ -2,7 +2,26 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to Rock-Paper-Scissors!</h1>
-  "
+  erb(:homepage)
+end
+
+get("/rock")do
+  allowed_moves = ["rock", "paper", "scissors"]
+  @computer_move = allowed_moves[rand(0..2)]
+  @player_move = "rock"
+  erb(:play_rock)
+end
+
+get("/paper")do
+  allowed_moves = ["rock", "paper", "scissors"]
+  @computer_move = allowed_moves[rand(0..2)]
+  @player_move = "paper"
+  erb(:play_paper)
+end
+
+get("/scissors")do
+  allowed_moves = ["rock", "paper", "scissors"]
+  @computer_move = allowed_moves[rand(0..2)]
+  @player_move = "scissors"
+  erb(:play_scissors)
 end
